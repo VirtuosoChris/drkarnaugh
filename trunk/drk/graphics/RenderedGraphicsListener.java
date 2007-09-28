505ef1b8edbe3fe49b97c38fa3bd830c9f4541ab
steve@soapforge.com <steve@soapforge.com@aa802f74-833a-0410-b846-4b95d8cac55e>
2007-09-28 15:25:41 +0000

    x

--- trunk/drk/graphics/RenderedGraphicsListener.java
+++ trunk/drk/graphics/RenderedGraphicsListener.java
@@ -1,13 +1,42 @@
 package drk.graphics;
-public class RenderedGraphicsListener {/*implements GLEventListener
-{
+import javax.swing.*;
+import javax.media.opengl.*;
+import java.awt.*;
+import com.sun.opengl.util.*;
 
-	public void main(String [] args)
+public abstract class RenderedGraphicsListener implements GLEventListener
+{
+	public void reshape(GLAutoDrawable drawable,int x,int y,int width,int height)	//is xyheight order correct?
+	{
+		
+	}
+	
+	public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged)
+	{
+		
+	}
+	public void doMain(String [] args)
 	{
 		JFrame jf=new JFrame("Dr. Karnaugh's Lab");
-		jf.add(new GLJPanel(new RenderedGraphicsListener()));
+		GLCanvas ad = new GLCanvas();
+		
+		try
+		{
+			ad.addGLEventListener(this);
+		}
+		catch(Exception e)
+		{
+			System.err.println(e);
+			//#ERROR
+		}
+		
+		Animator anim=new Animator(ad);
+		jf.getContentPane().add(ad);
+		jf.setVisible(true);
+		jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		while(!jf.isShowing());
+		anim.start();
 		//jf
 	}
 	
-}*/
 }
