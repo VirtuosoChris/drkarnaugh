1ff094105dbce7e91f74832db24d2ee090aa1b86
steve@soapforge.com <steve@soapforge.com@aa802f74-833a-0410-b846-4b95d8cac55e>
2007-10-17 21:26:08 +0000

    Stuff

---
+++ trunk/drk/graphics/particle/ParticleSystem.java
@@ -0,0 +1,77 @@
+package drk.graphics.particle;
+
+import javax.media.opengl.GL;
+
+import drk.DeltaTimer;
+import drk.graphics.GLInitializable;
+
+public class ParticleSystem implements GLInitializable, TimeUpdatable
+{
+	DeltaTimer deltat;
+	Particle [] particles;
+	float []    poffsets;
+	NewtonMotion3D emittermotion;
+	public ParticleSystem(int n)
+	{
+		super();
+		particles=new Particle[n];
+		poffsets=new float[n*3];
+		emittermotion=new AccelerationMotion3D();
+		
+		
+		for(int i=0;i<n;i++)
+		{
+			particles[i]=new Particle()
+			{
+				public Particle()
+				{
+					motion=new VelocityMotion();
+				}
+			}
+		}
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	public void initialize(GL gl)
+	{
+	// TODO Auto-generated method stub
+
+	}
+
+	public void render(GL gl)
+	{
+		gl.glPushMatrix();
+		gl.glTranslated(0.0,1.0,0.0);
+		gl.glBegin(GL.GL_POINTS);
+		{
+			gl.glColor3f(1.0f,0.0f,1.0f);
+			for(Particle p:particles)
+			{
+				gl.glVertex3d(p.)
+			}
+		}
+		gl.glEnd();
+	// TODO Auto-generated method stub
+
+	}
+
+	public void setDeltaTimer(DeltaTimer dt)
+	{
+		deltat=dt;
+		for(Particle p:particles)
+			p.update();
+		
+	// TODO Auto-generated method stub
+
+	}
+
+	public void update()
+	{
+		for(Particle p:particles)
+			p.update();
+	// TODO Auto-generated method stub
+
+	}
+
+}
