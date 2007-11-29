54fe2e14f37da5e28c003c05b08638b8d8c7f6cc
Hwashburn428 <Hwashburn428@aa802f74-833a-0410-b846-4b95d8cac55e>
2007-11-29 22:22:47 +0000

    Added this as a setup save load. You can work on this if you want.

---
+++ drk/game/SaveLoad.java
@@ -0,0 +1,54 @@
+package drk.game;
+import java.awt.event.KeyEvent;
+import java.io.*;
+import javax.media.opengl.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class SaveLoad extends KarnaughGame implements Serializable{
+	
+	protected KarnaughGame karGame;
+	
+	public void loadgame (String filename){
+		if(filename.length() > 0){
+			if (filename.indexOf(".karfig") < 1){
+				filename = filename.concat(".karfig");
+			}
+		
+			try{
+				FileInputStream fis = new FileInputStream(filename);
+				ObjectInputStream ois = new ObjectInputStream(fis);
+				karGame = (KarnaughGame)(ois.readObject());
+				ois.close();
+				fis.close();
+			}
+			catch(Exception e){
+				e.printStackTrace ();
+			}
+		}
+	}
+ 
+	public void savegame (String filename){
+		if (filename.length() > 0){
+			if (filename.indexOf(".karfig") < 1){
+				filename = filename.concat(".karfig");
+			}
+		
+			try{
+				FileOutputStream fos = new FileOutputStream(filename);
+				ObjectOutputStream oos = new ObjectOutputStream(fos);
+				oos.writeObject(karGame);
+				oos.close();
+				fos.close();
+			}
+			catch(Exception e){
+				e.printStackTrace ();
+			}
+		}
+	}
+	
+	public static void main(String args[]){
+			
+	}
+	
+}
