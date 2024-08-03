59f1b9393a290ebc2959fe601dfcb87ecc0878a5
Christopher Pugh <chris@virtuosoengine.com>
2024-08-03 01:41:30 -0400

    Compiles now from the command line
    javac -cp "lib/*" -sourcepath .\ -d .\bin .\drk\game\KarnaughGame.java
    
    Added jogl 1.1.1 from literally web archive lol

--- drk/circuit/BonusItem.java
+++ drk/circuit/BonusItem.java
@@ -1,42 +1,35 @@
 package drk.circuit;
+
 import drk.maze.MazeItem;
 import drk.game.KarnaughGame;
 import javax.media.opengl.*;
-public class BonusItem extends MazeItem{
 
-public boolean pickedUp=false;
+public class BonusItem extends MazeItem {
 
+    public boolean pickedUp = false;
 
-public String toString(){
-	return "BonusItem";
-}
+    public String toString() {
+        return "BonusItem";
+    }
 
+    public void onMazeItemHighlighted(KarnaughGame k) {
 
+        if (!pickedUp) {
 
-public void onMazeItemHighlighted(KarnaughGame k){
+            if (k.leftClick || k.rightClick) {
+                k.Score += 25;
+                pickedUp = true;
+            }
 
+            k.updateInfo("Pick up journals to learn about this place and increase your score");
+        }
 
-if(!pickedUp){
-	
-	if(k.leftClick || k.rightClick){
-	k.Score +=25;
-	pickedUp = true;
-	}
-	
-	k.updateInfo("Pick up journals to learn about this place and increase your score");
-}
+    }
 
-}
-
-
-
-public void render(GL gl){
-	if(!pickedUp)
-	{
-		super.render(gl);
-	}
-}
-	
-
+    public void render(GL gl) {
+        if (!pickedUp) {
+            super.render(gl);
+        }
+    }
 
 }
